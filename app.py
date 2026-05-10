9ab5d08a80f85830e64e6b7510e0c4889af6489a
Nirmal Maurya <mauryanirmal428@gmail.com>
2026-05-10 11:55:49 +0530

    Fix index  folder issue

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-
-
 import os
 from flask import Flask, render_template
 
-app = Flask(__name__, 
-            static_folder=os.path.join(os.path.dirname(__file__), 'static'),
-            template_folder=os.path.join(os.path.dirname(__file__), 'templates'))
+app = Flask(
+    __name__,
+    static_folder="static",
+    static_url_path="/static",
+    template_folder="templates"
+)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-
-if __name__ =='__main__':
-    app.run(debug=True,port=5002)
+if __name__ == "__main__":
+    app.run(debug=True)
